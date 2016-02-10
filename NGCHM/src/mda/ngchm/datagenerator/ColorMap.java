d4881bad85f86aec0db7babf3bc4d9e439ce0551
mstucky <mstucky@insilico.us.com>
2016-02-10 14:04:08 -0500

    Some merge cleanup and a change to HeatMapGenerator to remove quotes from colorMap breakpoints when all breakpoints are numeric.

--- NGCHM/src/mda/ngchm/datagenerator/ColorMap.java
+++ NGCHM/src/mda/ngchm/datagenerator/ColorMap.java
@@ -16,16 +16,38 @@ public class ColorMap {
 		StringBuffer json = new StringBuffer(TAB+TAB+QUOTE+name+QUOTE+COLON+BRACE_OPEN+LINE_FEED);
 		json.append(TAB+TAB+TAB+TYPE_LABEL+QUOTE+type+QUOTE+COMMA+LINE_FEED);
 		json.append(TAB+TAB+TAB+COLORS_LABEL+BRACKET_OPEN+QUOTE+HASHTAG+ toHex(colors.get(0))+QUOTE);
-		for (int i = 1; i < colors.size(); i++) {json.append(COMMA+QUOTE+HASHTAG+toHex(colors.get(i))+QUOTE);}
+		for (int i = 1; i < colors.size(); i++) {
+			json.append(COMMA+QUOTE+HASHTAG+toHex(colors.get(i))+QUOTE);
+		}
 		json.append(BRACKET_CLOSE+COMMA+LINE_FEED);
-		json.append(TAB+TAB+TAB+THRESHOLDS_LABEL+BRACKET_OPEN+QUOTE+breaks.get(0)+QUOTE);
-		for (int i = 1; i < breaks.size(); i++) {json.append(COMMA+QUOTE+breaks.get(i)+QUOTE);}
+		boolean isNumeric = areBreaksNumeric(breaks);
+		json.append(TAB+TAB+TAB+THRESHOLDS_LABEL+BRACKET_OPEN+getBreakString(breaks.get(0), isNumeric));
+		for (int i = 1; i < breaks.size(); i++) {
+			json.append(COMMA+getBreakString(breaks.get(i), isNumeric));
+		}
 		json.append(BRACKET_CLOSE+COMMA+LINE_FEED);
 		json.append(TAB+TAB+TAB+MISSING_LABEL+QUOTE+HASHTAG+toHex(missingColor)+QUOTE+LINE_FEED);
 		json.append(TAB+TAB+BRACE_CLOSE);
 		return json.toString();
 	}
 	
+	private String getBreakString(String breakpt, boolean isNumeric) {
+		if (!isNumeric) {
+			breakpt = QUOTE+breakpt+QUOTE;
+		}
+		return breakpt;
+	}
+
+	private boolean areBreaksNumeric(ArrayList<String> breaks) {
+		boolean isNumeric = true;
+		for (int i = 0; i < breaks.size(); i++) {
+			if (!HeatmapDataGenerator.isNumeric(breaks.get(i))) {
+				isNumeric = false;
+			}
+		}
+		return isNumeric;
+	}
+
 	private String toHex(Color c) {
 		String hex = "" + Integer.toHexString(c.getRGB());
 		return hex.substring(2);
