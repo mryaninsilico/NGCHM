6c29ce51f0c634fe8cf3de846dc58ea63b0ccddd
Michael Ryan <mryan@insilico.us.com>
2016-03-09 09:06:23 -0500

    Updates to GalaxyMapGen for newer heatmap.properties format and classification bars.

--- NGCHM/src/mda/ngchm/datagenerator/GalaxyMapGen.java
+++ NGCHM/src/mda/ngchm/datagenerator/GalaxyMapGen.java
@@ -6,21 +6,23 @@ import java.util.Date;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 
 /* Wrapper class to connect the HeatmapDataGenerator into a Galaxy tool */
 public class GalaxyMapGen {
 
+
 	public static void main(String[] args){
 
-		String[] filenames = {"matrix_files", "row_dendro_file", "col_dendro_file","row_order_file","col_order_file", "classification_files", "N/A", "summary_method", "output_location" };
 
-		if (args.length < 9) {
-			System.out.println("Usage: GalaxyMapGen <matrix file> <row dendro file> <col dendro file> <row order file> <col order file> <row classificaiton file NOT USED> <col classification file NOT USED> <summary method> <output file>");
+		if (args.length < 8) {
+			System.out.println("Usage: GalaxyMapGen <matrix file> <row dendro file> <col dendro file> <row order file> <col order file> <summary method> [<classificaitons file> <classification_type>] <output file>");
 			System.exit(1);
 		}	
 		
@@ -31,40 +33,67 @@ public class GalaxyMapGen {
 		String subdir = name + File.separator + File.separator + "NGCHM";
 		File sub = new File(subdir);
 		sub.mkdir();
+		subdir = subdir + File.separator + File.separator;
 
-		String JSONlist = "{\n";
-		try(  PrintWriter fileout = new PrintWriter( "heatmapProperties.json", "UTF-8" )  ){
+		try {
+			PrintWriter fileout = new PrintWriter( "heatmapProperties.json", "UTF-8" );
+			fileout.println("{");
+			fileout.println("\t\"matrix_files\": [");
+			fileout.println("\t\t{");
+			fileout.println("\t\t\"name\": \"Data Layer 1\",");
+		 	fileout.println("\t\t\"path\":  \"" + args[0] + "\",");
+			fileout.println("\t\t\"color_type\": \"linear\",");
+			fileout.println("\t\t\"row_datatype\": \"None\",");
+			fileout.println("\t\t\"col_datatype\": \"None\",");
+			fileout.println("\t\t}");
+			fileout.println("\t],");
 
-			for ( int i = 0; i < args.length; i++ )              
-			{
-				if (i == 0) {
-					JSONlist = JSONlist + "\"" + filenames[i] + "\": [{\"matrix_title\": \"Matrix1\", \"matrix_name\": \"dl1\", \"matrix_file\": \"" + args[i] + "\",\"matrix_type\": \"linear\"}],\n";
-				} else if (i == 5 ) {	
-					JSONlist = JSONlist + "\"" +filenames[i] + "\": [ ],\n";
-				} else if (i == 6) {
-					//not a json parameter now.
-				} else if (i == 8) {
-					// use the created output directory for HeatmapDataGenerator output.
-					JSONlist = JSONlist + "\"" +filenames[i]+"\": \""+ subdir + File.separator + File.separator + "\",\n";
-				} else {
-					JSONlist = JSONlist + "\"" +filenames[i]+"\": \""+args[i] +"\",\n";
-				}
+			fileout.println("\t\"row_dendro_file\": \"" + args[1] + "\",");
+			fileout.println("\t\"col_dendro_file\": \"" + args[2] + "\",");
+			fileout.println("\t\"row_order_file\": \"" + args[3] + "\",");
+			fileout.println("\t\"col_order_file\": \"" + args[4] + "\",");
+
+
+			fileout.println("\t\"classification_files\": [");
+			for (int pos = 6; pos < args.length-1; pos+=2) {
+				String type = "column";
+				String colorType = "discrete";
+				if (args[pos+1].contains("row"))
+					type = "row";
+				if (args[pos+1].contains("continuous"))
+					colorType = "continuous";
+				fileout.println("\t\t{");
+				String fileName = new File(args[pos]).getName();
+				if (fileName.contains("."))
+					fileName = fileName.substring(0,fileName.lastIndexOf("."));
+				fileout.println("\t\t\"name\": \"" + fileName + "\",");
+				fileout.println("\t\t\"path\": \"" + args[pos] + "\",");
+				fileout.println("\t\t\"color_type\": \"" + colorType + "\",");
+				fileout.println("\t\t\"position\": \"" + type + "\",");
+				if (pos == args.length-3) 
+					fileout.println("\t\t}");
+				else
+					fileout.println("\t\t},");	
 			}
-			JSONlist = JSONlist.substring(0,JSONlist.length()-1) +"\n}";
-			fileout.println( JSONlist );
+			fileout.println("\t],");
+
+			fileout.println("\t\"summary_method\": \"" + args[5] + "\",");
+			fileout.println("\t\"output_location\": \"" + subdir + "\",");
+			fileout.println("}");
+			
 			fileout.close();
 
 			String genArgs[] = new String[] {"heatmapProperties.json"};
 			HeatmapDataGenerator.main(genArgs);
 
 			//Zip results
-			zipDirectory(theDir, args[8]);
+			zipDirectory(theDir, args[args.length-1]);
 			
 		} catch(Exception e) {
-			System.out.println( "error in Init_Blocks e= "+ e);
+			e.printStackTrace();
+			System.out.println( "error in GalaxyMapGen e= "+ e.getMessage());
 		}
 
-		System.out.print(JSONlist);
 
 	}
 
